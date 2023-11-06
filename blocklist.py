cc04fe7a663d1ff2e70cf420af2f342a86dacdae
khaledm1 <khaledm1@vmware.com>
2023-11-06 14:42:13 +0200

    Flask store-items api

---
+++ blocklist.py
@@ -0,0 +1,1 @@
+BlockLIST = set()
---
+++ resources/store.py
@@ -0,0 +1,43 @@
+from flask.views import MethodView
+from flask_smorest import abort, Blueprint
+from schemas import StoreSchema
+from models import StoreModel
+from db import db
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+
+blp = Blueprint("stores", __name__, description="Operation in stores")
+
+
+@blp.route("/store/<int:store_id>")
+class Store(MethodView):
+    @blp.response(200, StoreSchema)
+    def get(self, store_id):
+        return StoreModel.query.get_or_404(store_id)
+
+    def delete(self, store_id):
+        store = StoreModel.query.get_or_404(store_id)
+        db.session.delete(store)
+        db.session.commit()
+        return {"message": "Store Deleted."}
+
+
+@blp.route("/store")
+class StoreList(MethodView):
+    @blp.response(200, StoreSchema(many=True))
+    def get(self):
+        return StoreModel.query.all()
+
+    @blp.arguments(StoreSchema)
+    @blp.response(201, StoreSchema)
+    def post(self, store_data):
+        store = StoreModel(**store_data)
+
+        try:
+            db.session.add(store)
+            db.session.commit()
+        except IntegrityError:
+            abort(400, message="A store already exists.")
+        except SQLAlchemyError:
+            abort(500, message="An Error occurred when inserting the store")
+
+        return store
